73cdff06f2a60080a8f469205742547f171cfda0
Vincent Chalnot <vincent.chalnot@gmail.com>
2018-05-23 18:22:14 +0200

    Adding translation decorator to ignore false translation domains

---
+++ Translator/TranslatorDecorator.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Sidus\BaseBundle\Translator;
+
+use Symfony\Component\Translation\Exception\InvalidArgumentException;
+use Symfony\Component\Translation\MessageCatalogueInterface;
+use Symfony\Component\Translation\TranslatorBagInterface;
+use Symfony\Component\Translation\TranslatorInterface;
+
+/**
+ * Overrides base translator to ignore translations when domain is false
+ */
+class TranslatorDecorator implements TranslatorInterface, TranslatorBagInterface
+{
+    /** @var TranslatorInterface */
+    protected $translator;
+
+    /**
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function trans($id, array $parameters = [], $domain = null, $locale = null)
+    {
+        if (false === $domain) {
+            return strtr($id, $parameters);
+        }
+
+        return $this->translator->trans($id, $parameters, $domain, $locale);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function transChoice($id, $number, array $parameters = [], $domain = null, $locale = null)
+    {
+        if (false === $domain) {
+            return strtr($id, $parameters);
+        }
+
+        return $this->translator->transChoice($id, $number, $parameters, $domain, $locale);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setLocale($locale)
+    {
+        $this->translator->setLocale($locale);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLocale()
+    {
+        return $this->translator->getLocale();
+    }
+
+    /**
+     * Gets the catalogue by locale.
+     *
+     * @param string|null $locale The locale or null to use the default
+     *
+     * @return MessageCatalogueInterface
+     *
+     * @throws InvalidArgumentException If the locale contains invalid characters
+     */
+    public function getCatalogue($locale = null)
+    {
+        if ($this->translator instanceof TranslatorBagInterface) {
+            return $this->translator->getCatalogue($locale);
+        }
+
+        return null;
+    }
+}
