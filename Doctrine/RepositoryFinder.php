00cfed5d385a31d7d87283d9ed2da5fca7797991
Vincent Chalnot <vchalnot@clever-age.com>
2018-09-17 18:15:24 +0200

    New repository finder

---
+++ Doctrine/RepositoryFinder.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * This file is part of the Sidus/BaseBundle package.
+ *
+ * Copyright (c) 2015-2018 Vincent Chalnot
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sidus\BaseBundle\Doctrine;
+
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Simplified access to entities repository with proper exception management
+ *
+ * This class is not defined as a service so you can use this bundle without Doctrine
+ *
+ * @author Vincent Chalnot <vincent@sidus.fr>
+ */
+class RepositoryFinder
+{
+    /** @var ManagerRegistry */
+    protected $doctrine;
+
+    /**
+     * @param ManagerRegistry $doctrine
+     */
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    /**
+     * @param string $className
+     *
+     * @return EntityRepository
+     */
+    public function getRepository($className)
+    {
+        $entityManager = $this->doctrine->getManagerForClass($className);
+        if (!$entityManager instanceof EntityManagerInterface) {
+            throw new \UnexpectedValueException("No manager found for class {$className}");
+        }
+        $repository = $entityManager->getRepository($className);
+        if (!$repository instanceof EntityRepository) {
+            throw new \UnexpectedValueException("No repository found for class {$className}");
+        }
+
+        return $repository;
+    }
+}
