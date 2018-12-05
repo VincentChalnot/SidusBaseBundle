726c69c40480c41d4e17c825998bfe4f0e357b30
Vincent Chalnot <vchalnot@clever-age.com>
2018-12-05 19:42:52 +0100

    New SleepUtility Simplifies the job of outputing proper values with __sleep() magic method

---
+++ Utilities/SleepUtility.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * This file is part of the Sidus/BaseBundle package.
+ *
+ * Copyright (c) 2015-2018 Vincent Chalnot
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sidus\BaseBundle\Utilities;
+
+/**
+ * Use this class to output the proper values for the magic __sleep method when you want to exclude certain properties
+ *
+ * @author Vincent Chalnot <vincent@sidus.fr>
+ */
+class SleepUtility
+{
+    /**
+     * @param string $class
+     * @param array  $excludedProperties
+     *
+     * @return array
+     */
+    public static function sleep($class, array $excludedProperties = [])
+    {
+        $propertyNames = [];
+        $refl = new \ReflectionClass($class);
+        foreach ($refl->getProperties() as $property) {
+            if (\in_array($property->getName(), $excludedProperties, true)) {
+                continue;
+            }
+            $propertyNames[] = $property->getName();
+        }
+
+        return $propertyNames;
+    }
+}
